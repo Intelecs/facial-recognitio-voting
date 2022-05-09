ff3541cf9972a54e1951391a9946db09a6e32e10
JanuaryThomas <january_thomas@hotmail.com>
2022-05-09 07:17:05 +0300

    updating plate number

--- plate-number/detect.py
+++ plate-number/detect.py
@@ -45,29 +45,27 @@ else:
 for i in np.arange(0, detections.shape[2]):
     confidence = detections[0, 0, i, 2]
 
-    if confidence > 0.5:
+    if confidence > 0.2:
         idx = int(detections[0, 0, i, 1])
 
-        xLeftBottom = int(detections[0, 0, i, 3] * cols)
-        yLeftBottom = int(detections[0, 0, i, 4] * rows)
-        xRightTop = int(detections[0, 0, i, 5] * cols)
-        yRightTop = int(detections[0, 0, i, 6] * rows)
+        xLeftBottom = int(detections[0, 0, i, 3] * cols) - 20
+        yLeftBottom = int(detections[0, 0, i, 4] * rows) - 20
+        xRightTop = int(detections[0, 0, i, 5] * cols) - 4
+        yRightTop = int(detections[0, 0, i, 6] * rows)+ 20
         # cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
         #                   (0, 255, 255))
 
         # image[ystart:ystop, xstart:xstop]
 
-        croped = frame[yLeftBottom:yRightTop+10, xLeftBottom:xRightTop + 10]
+        croped = frame[yLeftBottom:yRightTop, xLeftBottom:xRightTop]
         print(classNames[idx])
 
         gray = cv2.cvtColor(croped, cv2.COLOR_BGR2GRAY)
         # gray = cv2.resize(gray, (600, 600))
         cv2.imwrite("croped.jpeg", gray)
 
-
-        
-
         image = Image.open("croped.jpeg")
+        # image = gray
         image = np.array(image)
         norm_img = np.zeros((image.shape[0], image.shape[1]))
         image = cv2.normalize(image, norm_img, 0, 255, cv2.NORM_MINMAX)
@@ -76,9 +74,15 @@ for i in np.arange(0, detections.shape[2]):
         
         text = pytesseract.image_to_string(image)
         results = pytesseract.image_to_data(image, output_type=Output.DICT)
-        print(results)
-        plate_text = pytesseract.image_to_string(image, lang='eng')
-        print(plate_text)
+        
+        plate_number = ""
+        text = results['text']
+        for i in range(len(text)):
+            if text[i] != '':                
+                plate_number += text[i].replace('\n', '').replace(' ', '').replace('-', '').replace('“', '')
+        
+        if len(plate_number) > 0:
+            print(plate_number[1:len(plate_number)])
         cv2.imshow("Frame", image)
         
         cv2.waitKey(0)
