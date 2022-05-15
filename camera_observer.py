fc405d09ca2cafbd2256aab9c95bbdf5b22c28f4
JanuaryThomas <january_thomas@hotmail.com>
2022-05-15 22:49:39 +0300

    adding camera observer

---
+++ camera_observer.py
@@ -0,0 +1,10 @@
+from device.FaceRecognition import FaceRecognition
+
+
+if __name__ == "__main__":
+    # FaceRecognition().faces_encoding()
+
+    face_recognizer = FaceRecognition(
+        encodings="encodings/encodings.pickle",
+    )
+    face_recognizer.faces_detection()
