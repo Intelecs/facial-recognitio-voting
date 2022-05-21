b09f7ac621011092197df4e094b8a38e32c44535
JanuaryThomas <january_thomas@hotmail.com>
2022-05-21 10:27:38 +0300

    addind absolute path

--- tests/test_fingerprint.py
+++ tests/test_fingerprint.py
@@ -1,4 +1,8 @@
 import unittest
+
+import os,sys
+sys.path.insert(0, os.path.dirname(os.getcwd()))
+
 from device.FingerprintSensor import FingerprintSensor
 from utils.utils import get_logger, get_serial_ports
 
