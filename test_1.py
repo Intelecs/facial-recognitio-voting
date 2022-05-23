0086e0b067cb4913e66f8c3731225357b9d48bd6
JanuaryThomas <january_thomas@hotmail.com>
2022-05-23 21:03:53 +0300

    adding finger print sensor

--- test_1.py
+++ test_1.py
@@ -14,8 +14,10 @@ for i in range(1, 20):
         fingerprint = FingerprintSensor(
             baudrate=9600*i,
         )
+        logger.info( baudrate=9600*i)
         fingerprint.setup_sensor()
-        logger.info( fingerprint.sensor_details(), baudrate=9600*i)
+        logger.info( fingerprint.sensor_details())
+        
     except Exception as e:
         logger.info(e)
         continue
