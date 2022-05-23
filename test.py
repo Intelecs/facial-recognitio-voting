26d534f47ee272b9fc3de889f2855e5959f26ee9
JanuaryThomas <january_thomas@hotmail.com>
2022-05-23 19:55:37 +0300

    adding circuit python

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ import adafruit_fingerprint
 # uart = busio.UART(board.TX, board.RX, baudrate=57600)
 
 # If using with a computer such as Linux/RaspberryPi, Mac, Windows with USB/serial converter:
-# import serial
+import serial
 uart = serial.Serial("/dev/ttyUSB0", baudrate=57600, timeout=1)
 
 # If using with Linux/Raspberry Pi and hardware UART:
