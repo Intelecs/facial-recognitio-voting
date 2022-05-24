854fd097004b64bd1023d7ca45f32355f19db89a
JanuaryThomas <january_thomas@hotmail.com>
2022-05-24 18:25:45 +0300

    updating serial

--- test_1.py
+++ test_1.py
@@ -12,12 +12,12 @@ ports = get_serial_ports()
 if __name__ == "__main__":
     for port, desc, hwid in sorted(ports):
         logger.info("{}: {} [{}]".format(port, desc, hwid))
-    serial_port = serial.Serial(port, baudrate=9600, timeout=0)
+    serial_port = serial.Serial(port, baudrate=9600, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
     with serial_port:
         while 1:
             # time.sleep(.001)
-            message = serial_port.readline()
-            message = message.decode()
+            message = serial_port.read(100)
+            message = message.decode("utf-8")
             message = message.strip()
             if message == "":
                 logger.info(f"Serial {message}")
