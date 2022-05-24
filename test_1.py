0946c147bd29690764a721431fbcf6ffe7c1cec3
JanuaryThomas <january_thomas@hotmail.com>
2022-05-24 21:49:10 +0300

    adding serial reader

--- test_1.py
+++ test_1.py
@@ -9,12 +9,19 @@ from utils.utils import get_logger, get_serial_ports
 logger =get_logger(name=__name__)
 
 ports = get_serial_ports()
+port = None
 
 if __name__ == "__main__":
     for port, desc, hwid in sorted(ports):
         logger.info("{}: {} [{}]".format(port, desc, hwid))
-    serial_port = serial.Serial("/dev/ttyUSB1", baudrate=9600)
-    serial_port.reset_input_buffer()
+        if "USB-Serial" in desc:
+            port = port
+            break
+    if port is not None:
+        logger.info("No USB-Serial port found")
+        
+        serial_port = serial.Serial(port, baudrate=9600)
+        serial_port.reset_input_buffer()
 
     while True:
         # serial_port.reset_input_buffer()
