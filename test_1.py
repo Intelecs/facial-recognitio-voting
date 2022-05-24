80649b7ab6a22423e3c83bc56ff4efc8d006038e
JanuaryThomas <january_thomas@hotmail.com>
2022-05-24 17:36:21 +0300

    adding serial

--- test_1.py
+++ test_1.py
@@ -2,7 +2,7 @@ import os,sys
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
-
+import serial
 from utils.utils import get_logger, get_serial_ports
 
 logger =get_logger(name=__name__)
@@ -12,4 +12,8 @@ ports = get_serial_ports()
 if __name__ == "__main__":
     for port, desc, hwid in sorted(ports):
         logger.info("{}: {} [{}]".format(port, desc, hwid))
+    serial_port = serial.Serial(port, baudrate=9600, timeout=1)
+    with serial_port:
+        logger.info(f"Serial {serial_port.readline()}")
+
 
