57b4e9e2edef9b6c1c37fef3893778ccbb18691e
JanuaryThomas <january_thomas@hotmail.com>
2022-05-24 18:53:59 +0300

    updating serial

--- test_1.py
+++ test_1.py
@@ -12,16 +12,16 @@ ports = get_serial_ports()
 if __name__ == "__main__":
     for port, desc, hwid in sorted(ports):
         logger.info("{}: {} [{}]".format(port, desc, hwid))
-    serial_port = serial.Serial("/dev/ttyUSB0", baudrate=9600)
+    serial_port = serial.Serial("/dev/ttyUSB1", baudrate=9600)
     serial_port.reset_input_buffer()
 
-    # while True:
-    #     serial_port.reset_input_buffer()
-    #     try:
-    #         data = serial_port.readline()
-    #         logger.info("data: {}".format(data))
-    #         if data:
-    #             logger.info(str(data))
-    #     except Exception as e:
-    #         logger.error(e)
-    #     time.sleep(0.1)
+    while True:
+        serial_port.reset_input_buffer()
+        try:
+            data = serial_port.readline()
+            logger.info("data: {}".format(data))
+            if data:
+                logger.info(str(data))
+        except Exception as e:
+            logger.error(e)
+        time.sleep(0.1)
