8b2f05f341830a01806c188aff3bd6e744d419e8
JanuaryThomas <january_thomas@hotmail.com>
2022-05-26 18:32:50 +0300

    adding html web socket

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from importlib import reload
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 import uvicorn
 import serial
 from starlette.websockets import WebSocket
+from starlette.responses import FileResponse 
+from starlette.staticfiles import StaticFiles
 import os, sys
 import multiprocessing
 import time
@@ -15,6 +18,11 @@ from utils.utils import get_logger, get_serial_ports
 app = Starlette(debug=True)
 logger =get_logger(name=__name__)
 ports = get_serial_ports()
+
+script_dir = os.path.dirname(__file__)
+st_abs_file_path = os.path.join(script_dir, "static/")
+# app.mount("/static", StaticFiles(directory=st_abs_file_path, html=True), name="static")
+
 _port = None
 serial_port = None
 
@@ -24,8 +32,16 @@ for port, desc, hwid in sorted(ports):
         _port = port
         break
 
-serial_port = serial.Serial(_port, baudrate=9600, timeout=0)
-serial_port.reset_input_buffer()
+try:
+    serial_port = serial.Serial(_port, baudrate=9600, timeout=0)
+    serial_port.reset_input_buffer()
+except Exception as e:
+    logger.error(e)
+    serial_port = None
+
+@app.route("/")
+async  def index(request):
+    return FileResponse(st_abs_file_path + "index.html")
 
 # @app.route("/send-finger/{id}")
 # async def send_fingerprint_id(request):
@@ -46,19 +62,30 @@ serial_port.reset_input_buffer()
 @app.websocket_route("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    serial_port.flushInput()
+    try:
+        serial_port.flushInput()
+        await websocket.send_json({"status": "Connected", "sensor": "finger_print"})
+    except Exception as e:
+        await websocket.send_json({"status": "Disconnected", "sensor": "finger_print"})
     try:
         while True:
 
             message = await websocket.receive_text()
+            await websocket.send_text("Hello from server!")
             if message.isnumeric():
                 serial_port.write(bytes(str(message), "utf-8"))
                 serial_port.flush()
-            elif serial_port.in_waiting > 0:
+            else:
+                logger.info(message)
+            
+            try:
+                if serial_port.in_waiting > 0:
                     data = serial_port.readline()
                     data = data.decode()
                     print(data)
                     await websocket.send_text(data)
+            except Exception as e:
+                pass
             
             # if serial_port.in_waiting > 0:
             #     logger.info("Reading from Serial Port")
@@ -74,5 +101,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 if __name__ == '__main__':
-    if serial_port.isOpen():
-       uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run("app:app", host='0.0.0.0', port=8000, reload=True)
