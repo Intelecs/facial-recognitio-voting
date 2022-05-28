febd5389ed97c70cb9a9ece1571df8a86110c3e9
JanuaryThomas <january_thomas@hotmail.com>
2022-05-28 14:36:14 +0300

    updating serial monitor

--- serial_monitor.py
+++ serial_monitor.py
@@ -6,7 +6,8 @@ import threading
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
 from utils.utils import get_logger, get_serial_ports
-import websockets
+import websocket
+websocket.enableTrace(True)
 
 logger =get_logger(name=__name__)
 ports = get_serial_ports()
@@ -23,29 +24,51 @@ for port, desc, hwid in sorted(ports):
 
 serial_port = serial.Serial(_port, baudrate=9600, timeout=0)
 
-async def socket_client():
-    async with websockets.connect('ws://localhost:5555/ws') as websocket:
+def on_message(ws, message):
+    def run(*args):
+        if message == 'R':
+            logger.info(f"Received message Socket {message}")
+            serial_port.write(b'R')
+            serial_port.flush()
+        if message.isnumeric():
+            serial_port.write(bytes(message, 'utf-8'))
+            serial_port.flush()
+            logger.info(f"Sendig message {message}")
+        ws.close()
+    threading.Thread(target=run).start()
+# async def socket_client():
+#     async with websockets.connect('ws://localhost:5555/ws') as websocket:
 
-        while True:
-            try:
-                message = await websocket.recv_nowait()
-                if message == 'R':
-                    logger.info(f"Received message Socket {message}")
-                    serial_port.write(b'R')
-                    serial_port.flush()
-                if message.isnumeric():
-                        serial_port.write(bytes(message, 'utf-8'))
-                        serial_port.flush()
-                        logger.info(f"Sendig message {message}")
-                if serial_port.inWaiting() > 0 :
-                        data = serial_port.readline()
-                        data = data.decode()
-                        logger.info("Received Data from Serial Port: {}".format(data))
-                        await websocket.send(data)
+#         while True:
+#             try:
+                
+#                 message = await websocket.recv()
+#                 if message == 'R':
+#                     logger.info(f"Received message Socket {message}")
+#                     serial_port.write(b'R')
+#                     serial_port.flush()
+#                 if message.isnumeric():
+#                         serial_port.write(bytes(message, 'utf-8'))
+#                         serial_port.flush()
+#                         logger.info(f"Sendig message {message}")
+#                 if serial_port.inWaiting() > 0 :
+#                         data = serial_port.readline()
+#                         data = data.decode()
+#                         logger.info("Received Data from Serial Port: {}".format(data))
+#                         await websocket.send(data)
                                         
-            except Exception as e:
-                logger.info(f"Erorr reading message {e}", exc_info=True)
+#             except Exception as e:
+#                 logger.info(f"Erorr reading message {e}", exc_info=True)
 
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(socket_client())
+    # asyncio.get_event_loop().run_until_complete(socket_client())
+    wsapp = websocket.WebSocketApp("'ws://localhost:5555/ws'", on_open=on_open, on_message=on_message, on_close=on_close)
+    wsapp.run_forever()
+
+    while True:
+        if serial_port.inWaiting() > 0 :
+            data = serial_port.readline()
+            data = data.decode()
+            logger.info("Received Data from Serial Port: {}".format(data))
+            wsapp.send(data)
